002faa5ca6a1358d01dbb9d266c91d12cd157717
Daniele <daniele.saretta@dblu.it>
2021-04-13 13:45:17 +0200

    Fix Compab loading problem

--- dblu.Plugin.Docs/Class/SFPdf.cs
+++ dblu.Plugin.Docs/Class/SFPdf.cs
@@ -352,8 +352,23 @@ namespace dblu.Portale.Plugin.Docs.Class
                         //funzione merge
                         try
                         {
-
-                            PdfDocumentBase.Merge(finalDoc, ListaPdf.ToArray());
+                            foreach (Stream s in ListaPdf)
+                            {
+                                s.Position = 0;
+                                PdfLoadedDocument l = new PdfLoadedDocument(s);
+                                for (int q = 0; q < l.PageCount; q++)
+                                {
+                                    try
+                                    {
+                                        finalDoc.ImportPage(l, q);
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        _logger.LogError($"Unable to import page {q}, skipping it");
+                                    }
+                                }
+                            }
+                           
                             //creazione stram per il file finale
                             //FileStream fileStreamMerge = new FileStream(NomePdf, FileMode.CreateNew, FileAccess.ReadWrite);
                             using (FileStream fileStreamMerge = new FileStream(NomePdf, FileMode.CreateNew, FileAccess.ReadWrite))
@@ -846,6 +861,7 @@ namespace dblu.Portale.Plugin.Docs.Class
                 try
                 {
                     ld = new PdfLoadedDocument(m);
+                    if (ld.PageCount == 0) throw new Exception();
                 }
                 catch 
                 {
