e65409eb4d462f7d70407e69513a9b87ba77ba9b
Dimitri Battagion <dimitri.battagion@dblu.it>
2021-04-20 18:39:30 +0200

    viewModel

---
+++ dblu.Plugin.Docs/ViewModels/ImgViewModel.cs
@@ -0,0 +1,30 @@
+﻿using BPMClient;
+using dblu.Docs.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dblu.Portale.Plugin.Docs.ViewModels
+{
+    public class ImgViewModel
+    {
+        public string IdAllegato { get; set; }
+        public string NomeFile { get; set; }
+        public bool isRelated { get; set; }
+        public ImgViewModel()
+        {
+        }
+        public ImgViewModel(string IdAllegato, string NomeFile, bool isRelated)
+        {
+            this.IdAllegato = IdAllegato;
+            this.NomeFile = NomeFile;
+            this.isRelated = isRelated;
+        }
+
+    }
+
+
+
+}
